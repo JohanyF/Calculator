e2a08771d0d828a497db9e0544421e2cf8ef11a3
JohanyF <johanyf627@gmail.com>
2023-08-05 17:10:32 -0500

    Added four matho operator functions.

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = (operator1, operator2) => {
+    return operator1 + operator2;
+};
+
+const subtract = (operator1, operator2) => {
+    return operator1 - operator2;
+};
+
+const multiply = (operator1, operator2) => {
+    return operator1 * operator2;
+};
+
+const divide = (operator1, operator2) => {
+    return operator1 / operator2;
+};
+
+ 
