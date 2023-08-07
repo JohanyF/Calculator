716640cde00648a6d6f4a9195e6baf043169adb3
JohanyF <johanyf627@gmail.com>
2023-08-07 18:38:10 -0500

    Added displayNumbers arrow function and added functionality to all numeric buttons.

--- script.js
+++ script.js
@@ -30,3 +30,59 @@ const operate = (operator, operand1, operand2) => {
             break;
     }
 };
+
+const displayNumbers = (test) => {
+    const operations = document.querySelector(".operations");
+    operations.textContent += test;
+    
+    operandOne = operations.textContent;
+
+};
+
+
+
+let operandOne;
+let operandTwo;
+let operatorChoice;
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
+
+zero.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent);
+});
+one.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent);
+});
+two.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent)
+});
+three.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent)
+});
+four.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent)
+});
+five.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent)
+});
+six.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent)
+});
+seven.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent)
+});
+eight.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent)
+});
+nine.addEventListener("click", function (e) {
+    displayNumbers(e.target.textContent)
+});
