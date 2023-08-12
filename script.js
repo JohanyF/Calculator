e3ca477817acfaad188b5e36e27475a71822164b
JohanyF <johanyf627@gmail.com>
2023-08-11 23:32:57 -0500

    Remove return keyword from add, subtract, multiply, and divide functions.

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-const add = (operand1, operand2) => {
-    return operand1 + operand2;
-};
-
-const subtract = (operand1, operand2) => {
-    return operand1 - operand2;
-};
-
-const multiply = (operand1, operand2) => {
-    return operand1 * operand2;
-};
-
-const divide = (operand1, operand2) => {
-    return operand1 / operand2;
-};
-
+const add = (operand1, operand2) => operand1 + operand2;
+const subtract = (operand1, operand2) => operand1 - operand2;
+const multiply = (operand1, operand2) => operand1 * operand2;
+const divide = (operand1, operand2) => operand1 / operand2;
 const mod = (operand1, operand2) => operand1 % operand2;
     
-
 const operate = (operator, operand1, operand2) => {
-
     switch(operator) {
         case "+":
             calculatorAnswer = add(Number(operand1), Number(operand2));
